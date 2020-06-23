4c35da84de99d34111edfefea5b70b7a0841b433
Daniel Grebe <danieljgrebe@gmail.com>
2020-06-23 17:28:37 -0400

    Bug fixes

--- vibes/modelclass.py
+++ vibes/modelclass.py
@@ -7,18 +7,18 @@ class DataVibes:
 
     """
 
-    def __init__(self, datafile, funcfile=DEFAULT_HPTFX):
+    def __init__(self, datafile, type='csv', funcfile=DEFAULT_HPTFX):
         """
         Initialise la classe de DataVibes
         :param tempfile: fichier des données temporelles
         :param funcfile: fichier des fonctions de transformation
         """
-        import_func = functions.import_csv
+        import_func = transform.ImportFile(type=type)
         self.transformations = [[import_func, import_func(datafile)]]
 
     def add_transformation(self, cls, *args, **kwargs):
         func = cls(*args, **kwargs)
-        self.transformations.append([func, func(self.transformations[-1])])
+        self.transformations.append([func, func(self.transformations[-1][1])])
 
     def export_data(self):
         pass
