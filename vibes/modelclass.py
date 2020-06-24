b077a3eeb47cc16219b73fe6fc542f0257e3a036
Daniel Grebe <danieljgrebe@gmail.com>
2020-06-24 16:42:59 -0400

    Ajouter les fonctions insert_transformation et recalculate

--- vibes/modelclass.py
+++ vibes/modelclass.py
@@ -17,8 +17,37 @@ class DataVibes:
         self.transformations = [[import_func, import_func(datafile)]]
 
     def add_transformation(self, cls, *args, **kwargs):
+        """
+        Ajoute une transformation à la fin de la liste de transformations
+        :param cls: une classe de type Tranformation (mais pas ImportFile)
+        :param args: arguments pour l'initialisation de la classe cls
+        :param kwargs: arguments pour l'initialisation de la classe cls
+        :return:
+        """
         func = cls(*args, **kwargs)
         self.transformations.append([func, func(self.transformations[-1][1])])
+
+    def insert_transformation(self, cls, idx, *args, **kwargs):
+        """
+        Insert une transformation dans la liste des transformations
+        :param cls: une classe de type Tranformation (mais pas ImportFile)
+        :param idx: indice où insérer la nouvelle transformation dans la liste
+        :param args: arguments pour l'initialisation de la classe cls
+        :param kwargs: arguments pour l'initialisation de la classe cls
+        :return:
+        """
+        func = cls(*args, **kwargs)
+        self.transformations.insert(idx, [func, None])
+        self.recalculate(idx)
+
+    def recalculate(self, idx=0):
+        """
+        Recalculer les données dans la liste de transformations
+        :param idx: Endroit à partir du quel on recalcule
+        :return:
+        """
+        for i in range(idx, len(self.transformations)):
+            self.transformations[i][1] = self.transformations[i][0](self.transformations[i-1][1])
 
     def export_data(self):
         pass
