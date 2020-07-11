0776c7da1df2b22334527d670e02958971d596f1
Philippe <philippeboudreau99gmail.com>
2020-07-11 11:12:53 -0300

    rangeselectionfonctionne

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 control = ContLivesMatter.Controller("./test_files/test00.csv")
 #control.addTransform(None)
+control.data_range_selections(1,2)
 #data = vibes.DataVibes("./test_files/test00.csv")
 #data.add_transformation(vibes.ImportFile, "philippe")
 #data.insert_transformation(vibes.Filter, 1, None)
